8fe982aff47b00b10bf8258aa1a0e86db6c19922
ParthaDey5 <parthadey.hm@gmail.com>
2025-08-22 13:55:24 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+// tailwind.config.js
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
+export default {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  
+  theme: {
+    extend: {
+        screens: {
+          sm: '240px',
+          tablet: '801px',
+          xl: '1440px',
+        },
+
+      cursor: {
+        pointer: 'url("/icons/cursor4.png"), pointer'
+        
+      },
+      
+      colors: {
+        primary: {
+          blue950: 'hsl(213, 96%, 18%)',
+          purple600: 'hsl(243, 100%, 62%)',
+          blue300: 'hsl(228, 100%, 84%)',
+          blue200: 'hsl(206, 94%, 87%)',
+          red500: 'hsl(354, 84%, 57%)',
+        },
+        neutral: {
+          grey500: 'hsl(231, 11%, 63%)',
+          purple200: 'hsl(229, 24%, 87%)',
+          blue100: 'hsl(218, 100%, 97%)',
+          blue50: 'hsl(231, 100%, 99%)',
+          white: 'hsl(0, 100%, 100%)',
+        },
+      },
+      
+      
+      fontSize: {
+        base: '1rem',
+      },
+    
+    },
+    
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+    
+  ],
+};
