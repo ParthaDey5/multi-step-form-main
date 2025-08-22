ea3f356e1b35418bbb9792347670d9ddb9d37988
ParthaDey5 <parthadey.hm@gmail.com>
2025-08-22 14:14:56 +0530

    Update Confirmation.jsx

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import ThankYou from '/assets/images/icon-thank-you.svg?react';
 
 function Confirmation() {
   const [iconThankYou, setIconThankYou]= useState (false)
@@ -14,18 +14,18 @@ function Confirmation() {
     <div className='w-full tablet:aspect-square aspect-[1/1.2] flex flex-col items-center justify-center'>
         <div className='tablet:w-4/5 w-full flex flex-col items-center justify-center'>
 
-        <img className='tablet:mb-[1.5dvw] mb-[4dvw] tablet:w-fit w-[16dvw]' src="/assets/images/icon-thank-you.svg" alt="img" onLoad={loadIMG}/>
+        <ThankYou className='tablet:mb-[1.5dvw] mb-[4dvw] tablet:w-fit w-[16dvw]' />
       
         
-     {
+     
 
-     iconThankYou &&  <><h1 className='tablet:mb-[0.5dvw] mb-[1.5dvw]'>Thank you!</h1>
+     <h1 className='tablet:mb-[0.5dvw] mb-[1.5dvw]'>Thank you!</h1>
 
   <p className='tablet:w-[33dvw] w-[78dvw] text-center'><p className='tablet:hidden block mx-auto w-[78dvw]'>Thanks for confirming your subscription! </p><p className='tablet:hidden block mx-auto w-[70dvw]'>We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.</p>
   <p className='tablet:block hidden mx-auto'>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.</p>
   </p>
-  </>
-     }   
+  
+        
   </div>
   </div>
 
